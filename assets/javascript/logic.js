e27ff72974fedff13ed6cf1e6c8b4f10a176e1b9
Liam Condon <condonliamt@gmail.com>
2019-03-06 19:11:57 -0800

    added initial buttons on page load; not sure i like the look of them. Might decrease the number of them

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -2,20 +2,27 @@ $(document).ready(function() {
     var newButton; // to be given click listener
     var buttonArea = $("#buttons"); // to be appended on search enter
     var gifArea = $("#images");
-    var searchButton = $("#searchButton");
-    var clearButton = $("#clear");
-    var createButton; // to be assigned on click listener
-    var gifCount; // to be assigned value of number of results specified
+    var gifCount = 0; // to be assigned value of number of results specified
     var buttonColors = ["blue", "grey", "green", "orange", "cream", "yellow"];  // array of css classes that will be randomly assigned upon button creation;
-    var limit = "&limit=5";
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=";
-    var initialButtons = []; // initial buttons on page
+    var initialButtons = ["Tywinn", "Dooku", "Gandalf", "Dumbledore"]; // initial buttons on page
+
+    $(document).ready(function() {
+        for (var i=0; i < initialButtons.length; i++) {
+            newButton = $("<button>")
+            newButton.addClass(buttonColors[Math.floor(Math.random() * 6)]);
+            newButton.addClass("gif-button");
+            newButton.text(initialButtons[i]);
+            newButton.val(initialButtons[i]);    
+            buttonArea.append(newButton); 
+        }
+    })
 
     $(document).on('keypress',function(e) {
       if ((e.which == 13) && ($("#inputSearch").val() !== null)) {
         e.preventDefault()
         newButton = $("<button>")
-        newButton.addClass(buttonColors[Math.floor(Math.random() * 7)]);
+        newButton.addClass(buttonColors[Math.floor(Math.random() * 6)]);
         newButton.addClass("gif-button");
         newButton.text($("#inputSearch").val());
         newButton.val($("#inputSearch").val());    
@@ -24,6 +31,9 @@ $(document).ready(function() {
     })
 
   $(document).on("click", ".gif-button", function() {
+        $(".open").fadeOut();
+        $("#buttons").css("max-width", "840px");
+        $("#buttons").css("margin-bottom", "-=5px");
         gifArea.empty();
         var searchWord = $(this).val();
         console.log(searchWord);
