193a7e607363f5f4c2717d25bcf5e571ed9481fc
Liam Condon <condonliamt@gmail.com>
2019-03-06 17:07:25 -0800

    fixed click listener and ajax call

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -6,32 +6,44 @@ $(document).ready(function() {
     var clearButton = $("#clear");
     var createButton; // to be assigned on click listener
     var gifCount; // to be assigned value of number of results specified
-    var buttonColors = ["blue", "grey", "green", "orange", "cream"];  // array of css classes that will be randomly assigned upon button creation;
+    var buttonColors = ["blue", "grey", "green", "orange", "cream", "yellow"];  // array of css classes that will be randomly assigned upon button creation;
     var limit = "&limit=5";
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=";
     var initialButtons = []; // initial buttons on page
 
     searchButton.click(function() {
         newButton = $("<button>")
-        newButton.addClass(buttonColors[Math.floor(Math.random() * 6)]);
+        newButton.addClass(buttonColors[Math.floor(Math.random() * 7)]);
         newButton.addClass("gif-button");
+
         newButton.text($("#inputSearch").val());
         newButton.val($("#inputSearch").val());    
         buttonArea.append(newButton);
     })
+  $(document).on("click", ".gif-button", function() {
 
-    $(".gif-button").click(function() {
         var searchWord = $(this).val();
         console.log(searchWord);
+        $("#book-container").attr("id", "pages-container")
         $.ajax({
-            url: queryURL + searchWord + "t8uKuW8SvPkDbPqFlCUt7GyWA5IkhH5M&limit=4",
+            url: queryURL + searchWord + "&api_key=t8uKuW8SvPkDbPqFlCUt7GyWA5IkhH5M&limit=4",
             method: "GET",
         }).then(function(response){
         for (var i = 0; i < 5; i++)
             console.log(response);
-            var gifsToAdd = response.data[i].images.fixed_height.url
-            console.log(gifsToAdd);
-            gifArea.append(gifsToAdd);
+            var gifToAdd = $("<img>").attr(response.data[i].images.fixed_height_still.url);
+            console.log(gifToAdd);
+            gifArea.append(gifToAdd);
         })
+
+    var state = $(this).attr("data-state");
+     
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
     })
-})
